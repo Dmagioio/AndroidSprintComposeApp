085d24bb52234910ac5eea948c4e8a2186a1378d
marki <markian3838@gmail.com>
2025-11-17 14:11:52 +0200

    learn scaffold and @preview

--- app/src/main/java/com/example/androidsprintcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/androidsprintcomposeapp/MainActivity.kt
@@ -4,44 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidsprintcomposeapp.ui.theme.AndroidSprintComposeAppTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AndroidSprintComposeAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+            Scaffold { padding: PaddingValues ->
+                StudyAppHeader(padding)
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun StudyAppHeader(padding: PaddingValues) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Android text",
+        modifier = Modifier.padding(padding),
+        fontSize = 28.sp
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AndroidSprintComposeAppTheme {
-        Greeting("Android")
-    }
+private fun StudyAppHeaderPreview(){
+    StudyAppHeader(padding = PaddingValues())
 }
