bad16fe81a301d9d680ff15a184405e754116485
marki <markian3838@gmail.com>
2025-11-23 00:25:36 +0200

    Count declared with BY

--- app/src/main/java/com/example/androidsprintcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/androidsprintcomposeapp/MainActivity.kt
@@ -4,48 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ripple.rememberRipple
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.androidsprintcomposeapp.ui.theme.AndroidSprintComposeAppTheme
-import com.example.androidsprintcomposeapp.ui.theme.mainTitleColor
-import com.example.androidsprintcomposeapp.ui.theme.subtitle
-import com.example.androidsprintcomposeapp.ui.theme.subtitleGrayColor
-import kotlinx.coroutines.runInterruptible
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,16 +30,17 @@ class MainActivity : ComponentActivity() {
 fun BananaText(){
     println("Banana button starts")
 
-    val count = remember {
+    var count by remember {
+        println("remember")
         mutableStateOf(0)
     }
 
     println("Count is declared")
 
     Text(
-        "Count is: ${count.value}",
+        "Count is: ${count}",
         modifier = Modifier.clickable{
-            count.value++
+            count++
         },
         style = TextStyle(fontSize = 44.sp)
     )
